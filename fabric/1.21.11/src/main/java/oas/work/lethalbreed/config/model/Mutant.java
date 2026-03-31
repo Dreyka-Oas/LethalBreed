2b467a3f016c89ee07d0d516989f5cc0786e37fa
Dreyka Oas <dreyka@oas.work>
2026-03-31 21:15:23 +0200

    Remove fabric folder from main - docs only

--- fabric/1.21.11/src/main/java/oas/work/lethalbreed/config/model/Mutant.java
+++
@@ -1,8 +0,0 @@
-package oas.work.lethalbreed.config.model;
-
-public class Mutant {
-    public String _comment = "Mutant boss settings and minion spawning";
-    public float mutantChance = 0.05f;
-    public int mutantMinionCount = 8;
-    public int mutantTentacleTickRate = 5;
-}
